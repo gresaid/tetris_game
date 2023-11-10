9783165e5c5e250dc8a0a1def69d8cb12360492c
pfc <gresvladimir02@gmail.com>
2023-11-10 15:06:23 +0600

    + check_hit + set_piece

--- tetris_oop/tetris_oo.cc
+++ tetris_oop/tetris_oo.cc
@@ -23,10 +23,54 @@ private:
   int board[20][10];
   int score = 0;
   int shapes[7][4] = {{x, y, x, y},
-                 {r, p, r, p},
-                 {c, c, c, c},
-                 {599636, 431376, 598336, 432192},
-                 {411985, 610832, 415808, 595540},
-                 {px, py, px, py},
-                 {614928, 399424, 615744, 428369}};
+                      {r, p, r, p},
+                      {c, c, c, c},
+                      {599636, 431376, 598336, 432192},
+                      {411985, 610832, 415808, 595540},
+                      {px, py, px, py},
+                      {614928, 399424, 615744, 428369}};
+
+  int
+  NUM(int x, int y) {
+    return 3 & shapes[p][x] >> y;
+  }
+
+  int
+  tick_rate() {
+    if (++tick > 30) {
+      tick = 0;
+      if (check_hit(x, y + 1, r)) {
+        if (!y) {
+          return 0;
+        }
+        remove_line();
+        new_piece();
+      } else {
+        y++;
+        update_piece();
+      }
+    }
+    return 1;
+  }
+
+  int
+  check_hit(int x, int y, int r) {
+    if (y + NUM(r, 18) > 19) {
+      return 1;
+    }
+    set_piece(px, py, pr, 0);
+    c = 0;
+    for (int i = 0; i < 8; i += 2) {
+      board[y + NUM(r, i * 2)][x + NUM(r, (i * 2) + 2)] && c++;
+    }
+    set_piece(px, py, pr, p + 1);
+    return c;
+  }
+
+  void
+  set_piece(int x, int y, int r, int v) {
+    for (int i = 0; i < 8; i += 2) {
+      board[NUM(r, i * 2) + y][NUM(r, (i * 2) + 2) + x] = v;
+    }
+  }
 };
